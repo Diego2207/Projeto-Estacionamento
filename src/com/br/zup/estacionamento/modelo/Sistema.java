7d6d2c61c5a1d5296f77999c682b0ea5f329c7d4
Diego2207 <diego.santos@zup.com.br>
2019-06-29 00:38:56 -0300

    Adicionar projeto estacionamento

--- src/com/br/zup/estacionamento/modelo/Sistema.java
+++ src/com/br/zup/estacionamento/modelo/Sistema.java
@@ -1,89 +1,117 @@
 package com.br.zup.estacionamento.modelo;
 
-import java.util.Calendar;
 import java.util.Scanner;
 
-//teste
 public class Sistema {
+	
+	private double horaEntrada = 0;
+	private double horaSaida = 0;
+	private double precoPeriodo = 10;
+	private double precoAdicional = 5;
 
-	public static void main(String[] args) {
-		
-		//Criar carro
-		Carro carZup = new Carro();
-		
-		//Entrar com o carro
-		Calendar calendario = Calendar.getInstance();
-		calendario.set(Calendar.DAY_OF_MONTH, 1);
-		calendario.set(Calendar.HOUR_OF_DAY, 10);
-		calendario.set(Calendar.MINUTE, 20);
-		
-		//Registra a hora de entrada
-		carZup.setEntrada(calendario.getTime());
-		//mostrar o horario de entrado do carro
-		System.out.println("O carro entrou as " + carZup.getEntrada());
-		
-		//Sair com o carro 
-		Calendar calendario2 = Calendar.getInstance();
-		//Registra a hora de saida
-		calendario2.set(Calendar.DAY_OF_MONTH, 1);
-		calendario2.set(Calendar.HOUR_OF_DAY, 10);
-		calendario2.set(Calendar.MINUTE, 36);
-		carZup.setSaida(calendario2.getTime());
-		//mostrar o horario de saida 
-		System.out.println("O carro saiu as " + carZup.getSaida());
-		
-		//Mostrar quanto tempo ele ficou estacionado
-		//Criar objeto Caixa
-		Caixa caixaShopping = new Caixa();
-		long tempoNoShopping = (long)caixaShopping.calcularTempoParadoNoEstacionamento(carZup.getEntrada(), carZup.getSaida());
-		System.out.println("O carro ficou " + tempoNoShopping + " minutos");
-		//Mostrar quanto custou a saida
-		double valorPago = caixaShopping.calcularQuantoVouPagarNoEstacionamento(tempoNoShopping);
-		System.out.println("Foi pago o valor de " + valorPago);
-		
-		
-		//Fazendo com interacao do user
-		//Pegando dados da entrada
-		Moto motoZuper = new Moto();
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Que dia voce entrou? ");
-		byte dia = sc.nextByte();
-		System.out.println("Que hora voce entrou? ");
-		byte hora = sc.nextByte();
-		System.out.println("Que min voce entrou? ");
-		byte min = sc.nextByte();
-		
-		Calendar calendarioMoto = Calendar.getInstance();
-		calendarioMoto.set(Calendar.DAY_OF_MONTH, dia);
-		calendarioMoto.set(Calendar.HOUR_OF_DAY, hora);
-		calendarioMoto.set(Calendar.MINUTE, min);
-		
-		motoZuper.setEntrada(calendarioMoto.getTime());
-		System.out.println("A moto entrou as " + motoZuper.getEntrada());
-		
-		//Pegando dados da saida
-		System.out.println("Que dia voce saiu? ");
-		dia = sc.nextByte();
-		System.out.println("Que hora voce saiu? ");
-		hora = sc.nextByte();
-		System.out.println("Que min voce saiu? ");
-		min = sc.nextByte();
-		
-		calendarioMoto.set(Calendar.DAY_OF_MONTH, dia);
-		calendarioMoto.set(Calendar.HOUR_OF_DAY, hora);
-		calendarioMoto.set(Calendar.MINUTE, min);
-		
-		motoZuper.setSaida(calendarioMoto.getTime());
-		System.out.println("A moto saiu as " + motoZuper.getSaida());
-		
-		tempoNoShopping = (long)caixaShopping.calcularTempoParadoNoEstacionamento(motoZuper.getEntrada(), motoZuper.getSaida());
-		System.out.println("A Moto ficou " + tempoNoShopping + " minutos");
-		//Mostrar quanto custou a saida
-		valorPago = caixaShopping.calcularQuantoVouPagarNoEstacionamento(tempoNoShopping);
-		System.out.println("Foi pago o valor de " + valorPago);
+	public Sistema(double horaEntrada, double horaSaida, double precoPeriodo, double precoAdicional) {
+		this.horaEntrada = horaEntrada;
+		this.horaSaida = horaSaida;
+		this.precoPeriodo = precoPeriodo;
+		this.precoAdicional = precoAdicional;
+	}
 
-		
-		
-		sc.close();
-	}	
+	public Sistema() {
+
+	}
+
+	public double getHoraEntrada() {
+		return horaEntrada;
+	}
+
+	public void setHoraEntrada(double horaEntrada) {
+		this.horaEntrada = horaEntrada;
+	}
+
+	public double getHoraSaida() {
+		return horaSaida;
+	}
+
+	public void setHoraSaida(double horaSaida) {
+		this.horaSaida = horaSaida;
+	}
+
+	public double getPrecoPeriodo() {
+		return precoPeriodo;
+	}
+
+	public void setPrecoPeríodo(double precoPeriodo) {
+		this.precoPeriodo = precoPeriodo;
+	}
+
+	public double getPrecoAdicional() {
+		return precoAdicional;
+	}
+
+	public void setPrecoAdicional(double precoAdicional) {
+		this.precoAdicional = precoAdicional;
+	}
+
+	public String toString() {
+		String modelo = "";
+
+		modelo += "Hora de entrada : " + this.getHoraEntrada()+ "\n";
+		modelo += "Hora de saida : " + this.getHoraSaida()+ "\n";
+
+		return modelo;
+	}
+
+	public Sistema inserirHoraEntrada() {
+
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		Sistema novaEntrada = new Sistema();
+
+		System.out.println("Digite a hora da entrada :");
+		double hora = scan.nextDouble();
+		novaEntrada.setHoraEntrada(hora);
+
+		return novaEntrada;
+
+	}
+
+	public void caculaValor(Sistema primeiraEntrada) {
+		Scanner scan = entrada();
+		Sistema sistema = primeiraEntrada;
+		double valor = 0;
+
+		System.out.println(sistema);
+
+		System.out.println("\nDigite a hora da saida :\n");
+		double saida = scan.nextDouble();
+		sistema.setHoraSaida(saida);
+
+		System.out.println(sistema);
+
+		double estadia = (sistema.getHoraSaida() - sistema.getHoraEntrada());
+
+		if (estadia <= 0.15) {
+			valor = 0;
+		} else if (estadia > 0.15 && estadia <= 1) {
+			valor = sistema.getPrecoPeriodo();
+		} else if (estadia > 1) {
+
+			double hora = Math.ceil(estadia - 1);
+			int horaAdicional = (int) hora;
+
+			valor = sistema.getPrecoPeriodo() + (horaAdicional * sistema.getPrecoAdicional());
+
+		}
+
+		System.out.println("Total de estadia :\n" + estadia);
+
+		System.out.println("\nTotal a pagar :\n" + valor);
+
+	}
+
+	public Scanner entrada() {
+		Scanner scan = new Scanner(System.in);
+		return scan;
+}
+
 }
